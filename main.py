0433c3d3ce0f1a0300c088abd9bfafcf5d135409
Doezer <doezerx@gmail.com>
2026-03-26 22:07:58 +0100

    refactor: streamline locale handling and configuration loading

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#  -*- coding: utf8 -*-
+# -*- coding: utf8 -*-
 import gettext
-import json
-import locale
 import logging
 import sys
 
 import src.cogs.core as core
+from src.config_utils import load_config
 from src.DiscordBot import DiscordBot
 
 __appname__ = "Scheduling Discord Bot"
@@ -13,45 +12,15 @@ __version__ = "0.1"
 __author__ = "Vincent 'Doezer' AIRIAU"
 
 
-def get_prompt():
-    prompt = "!"
+def set_locale(language: str):
+    filename = f"res/messages_{language[:2]}.mo"
     try:
-        with open("config.json", mode="r+", encoding="utf-8") as f:
-            config = json.load(f)
-            try:
-                prompt = config["prompt"]
-            except KeyError:
-                config.update({"prompt": prompt})
-                f.seek(0)
-                f.write(json.dumps(config))
-                f.truncate()
-            prompt = config["prompt"]
-    except Exception:
-        logging.debug("Prompt not found. Using default '{}'.".format(prompt))
-
-    logging.info("The prompt is {}.".format(prompt))
-    return prompt
-
-
-def set_locale():
-    try:
-        with open("config.json", mode="r+", encoding="utf-8") as f:
-            config = json.load(f)
-            language = config["language"]
-        locale.setlocale(locale.LC_ALL, language)
-    except Exception:
-        locale.setlocale(locale.LC_ALL, "")
-
-    loc = locale.getlocale()
-    filename = "res/messages_%s.mo" % locale.getlocale()[0][0:2]
-
-    try:
-        logging.debug("Opening message file %s for locale %s", filename, loc[0])
-        trans = gettext.GNUTranslations(open(filename, "rb"))
-    except IOError:
-        logging.debug("Locale not found. Using default messages")
+        logging.debug("Opening message file %s for language %s", filename, language)
+        with open(filename, "rb") as f:
+            trans = gettext.GNUTranslations(f)
+    except OSError:
+        logging.debug("Locale file not found. Using default messages.")
         trans = gettext.NullTranslations()
-
     trans.install()
 
 
@@ -61,24 +30,25 @@ def main():
         stream=sys.stdout,
         format="%(asctime)s,%(msecs)03d %(levelname)-5.5s [%(name)s] %(message)s <%(lineno)d>",
     )
-    prompt = get_prompt()
 
-    set_locale()
+    config = load_config()
+    prompt = config.get("prompt", "!")
+    language = config.get("language", "en")
+    token = config.get("token")
+
+    set_locale(language)
 
     cmd_list = {
-        "^{}help$".format(prompt): core.bot_help_embed,
-        "^{}(?:schedule)$".format(prompt): core.schedule_post,
-        "^{}(?:language)".format(prompt): core.language,
+        f"^{prompt}help$": core.bot_help_embed,
+        f"^{prompt}(?:schedule)$": core.schedule_post,
+        f"^{prompt}(?:language)": core.language,
     }
 
-    with open("config.json") as f:
-        config = json.load(f)
-
     bot = DiscordBot(prompt)
-    for action in cmd_list:
-        bot.register_action(action, cmd_list[action])
+    for action, handler in cmd_list.items():
+        bot.register_action(action, handler)
 
-    bot.run(config["token"])
+    bot.run(token)
 
 
 if __name__ == "__main__":
